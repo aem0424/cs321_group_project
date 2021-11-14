f86ad4cbdfc78bae63c4a752747ee68ef6a2c3bb
User <Anjan@DESKTOP-DVDQ58B>
2021-11-14 15:31:12 -0600

    connection test

---
+++ CS321Project/src/cs321project/Input.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs321project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Input {
+    /**
+      Construct phone object.
+      @param aScanner that reads text from a character-input stream
+   */
+   public Input(Scanner aScanner)
+   {
+      scanner = aScanner;
+   }
+
+   /**
+      Speak a message to System.out.
+      @param output the text that will be "spoken"
+   */
+   public void speak(String output)
+   {
+      System.out.println(output);
+   }
+
+   /**
+      Loops reading user input and passes the input to the
+      Connection object's methods dial, record or hangup.
+      @param c the connection that connects this phone to the
+      voice mail system
+   */
+   public void run(Connection c)
+   {
+      boolean more = true;
+      while (more)
+      {
+         String input = scanner.nextLine();
+         if (input == null) return;
+         if (input.equalsIgnoreCase("H"))
+            c.hangup();
+         else if (input.equalsIgnoreCase("Q"))
+            more = false;
+         else if (input.length() == 1
+            && "1234567890#".indexOf(input) >= 0)   //Change this
+            c.dial(input);
+         else
+            c.record(input);
+      }
+   }
+
+   private Scanner scanner;
+}
