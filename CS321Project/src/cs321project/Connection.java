dcbdd7f21e6e49b3756ec802bad75b21a2d882b4
User <Anjan@DESKTOP-DVDQ58B>
2021-11-14 15:25:45 -0600

    connection test

--- CS321Project/src/cs321project/Connection.java
+++ CS321Project/src/cs321project/Connection.java
@@ -6,9 +6,282 @@
 package cs321project;
 
 /**
- *
- * @author User
- */
-public class Connection {
-    
-}
+   Connects a phone to the mail system. The purpose of this
+   class is to keep track of the state of a connection, since
+   the phone itself is just a source of individual key presses.
+*/
+public class Connection
+{
+   /**
+      Construct a Connection object.
+      @param c a Cart object
+      @param i a Input object
+   */
+   public Connection(Cart c, Input i)
+   {
+      cart = c;
+      input = i;
+      resetConnection();
+   }
+
+   /**
+      Respond to the user's pressing a key on the phone touch pad
+      @param key the phone key pressed by the user
+   */
+   public void dial(String key)
+   {       
+      if (state == CONNECTED)
+         connect(key);
+      else if (state == RECORDING)
+         login(key);
+      else if (state == CHANGE_PASSCODE)
+         changePasscode(key);
+      else if (state == CHANGE_GREETING)
+         changeGreeting(key);
+      else if (state == MAILBOX_MENU)
+         mailboxMenu(key);
+      else if (state == MESSAGE_MENU)
+         messageMenu(key);
+      else if (state == FORWARD_MESSAGE)
+         forwardMessage(key);
+   }
+
+   /**
+      Record voice.
+      @param voice voice spoken by the user
+   */
+   public void record(String voice)
+   {
+      
+         currentRecording += voice;
+   }
+
+   /**
+      The user hangs up the phone.
+   */
+   public void hangup()
+   {
+      if (state == RECORDING)
+         currentMailbox.addMessage(new Message(currentRecording));
+      resetConnection();
+   }
+
+   /**
+      Reset the connection to the initial state and prompt
+      for mailbox number
+   */
+   private void resetConnection()
+   {
+      currentRecording = "";
+      accumulatedKeys = "";
+      state = CONNECTED;
+      phone.speak(INITIAL_PROMPT);
+   }
+
+   /**
+      Try to connect the user with the specified mailbox.
+      @param key the phone key pressed by the user
+   */
+   private void connect(String key)
+   {
+      if (key.equals("S"))
+      {         
+          currentMailbox = system.findMailbox(accumulatedKeys);  
+         if (currentMailbox != null)
+         {
+            state = RECORDING;
+            phone.speak(currentMailbox.getGreeting());
+         }
+         else
+            phone.speak("Incorrect mailbox number. Try again!");
+         accumulatedKeys = "";
+      }
+      else
+         accumulatedKeys += key;
+        
+   }
+
+   /**
+      Try to log in the user.
+      @param key the phone key pressed by the user
+   */
+   private void login(String key)
+   {
+      if (key.equals("#"))
+      {
+         if (currentMailbox.checkPasscode(accumulatedKeys))
+         {
+            state = MAILBOX_MENU;
+            phone.speak(MAILBOX_MENU_TEXT);
+         }
+         else
+            phone.speak("Incorrect passcode. Try again!");
+         accumulatedKeys = "";
+      }
+      else
+         accumulatedKeys += key;
+   }
+
+   /**
+      Change passcode.
+      @param key the phone key pressed by the user
+   */
+   private void changePasscode(String key)
+   {
+      if (key.equals("#"))
+      {
+         currentMailbox.setPasscode(accumulatedKeys);
+         state = MAILBOX_MENU;
+         phone.speak(MAILBOX_MENU_TEXT);
+         accumulatedKeys = "";
+      }
+      else
+         accumulatedKeys += key;
+   }
+
+   /**
+      Change greeting.
+      @param key the phone key pressed by the user
+   */
+   private void changeGreeting(String key)
+   {
+      if (key.equals("#"))
+      {
+         currentMailbox.setGreeting(currentRecording);
+         currentRecording = "";
+         state = MAILBOX_MENU;
+         phone.speak(MAILBOX_MENU_TEXT);
+      }
+   }
+
+   /**
+      Respond to the user's selection from mailbox menu.
+      @param key the phone key pressed by the user
+   */
+   private void mailboxMenu(String key)
+   {
+      if (key.equals("1"))
+      {
+         state = MESSAGE_MENU;
+         phone.speak(MESSAGE_MENU_TEXT);
+      }
+      else if (key.equals("2"))
+      {
+         state = CHANGE_PASSCODE;
+         phone.speak("Enter new passcode followed by the # key");
+      }
+      else if (key.equals("3"))
+      {
+         state = CHANGE_GREETING;
+         phone.speak("Record your greeting, then press the # key");
+      }
+   }
+
+   /**
+      Respond to the user's selection from message menu.
+      @param key the phone key pressed by the user 
+   */
+   private void messageMenu(String key)
+   {
+      if (key.equals("1"))
+      {
+         String output = "";
+         Message m = currentMailbox.getCurrentMessage();
+         if (m == null) output += "No messages." + "\n";
+         else output += m.getText() + "\n";
+         output += MESSAGE_MENU_TEXT;
+         phone.speak(output);
+      }
+      else if (key.equals("2"))
+      {
+         currentMailbox.saveCurrentMessage();
+         phone.speak(MESSAGE_MENU_TEXT);
+      }
+      else if (key.equals("3"))
+      {
+         currentMailbox.removeCurrentMessage();
+         phone.speak(MESSAGE_MENU_TEXT);
+      }
+      else if (key.equals("5"))
+      {
+         state = MAILBOX_MENU;
+         phone.speak(MAILBOX_MENU_TEXT);
+      }
+      else if (key.equals("4"))
+      {
+         if (currentMailbox.getCurrentMessage() != null)
+         {
+         state = FORWARD_MESSAGE;
+         phone.speak(FORWARD_TEXT);
+         }
+         
+         else
+         {
+             phone.speak("There are no messages to forward.");
+             phone.speak(MAILBOX_MENU_TEXT);
+         }
+      }
+    }
+   
+    /**
+      Forwards a message to another mailbox.
+      @param key phone key pressed by the user
+   */   
+    private void forwardMessage(String key)
+    {
+      if (key.equals("#"))
+      {         
+         Mailbox forwardMailbox = system.findMailbox(accumulatedKeys);
+         Message forwardM = currentMailbox.getCurrentMessage();
+         if (forwardMailbox != null)
+         {
+             try 
+             {
+                forwardMailbox.addMessage((Message)forwardM.cloneMessage());
+                phone.speak("Message forwarded to " + accumulatedKeys + ".");
+             } 
+             
+             catch (CloneNotSupportedException ex) 
+             {
+                phone.speak("Message cannot be forwarded.");
+             }
+             
+             state = MESSAGE_MENU;
+             phone.speak(MESSAGE_MENU_TEXT);
+         }
+         else
+            phone.speak("Incorrect mailbox number. Try again!");
+            accumulatedKeys = "";
+       }
+       else
+         accumulatedKeys += key;
+    }
+
+   private MailSystem system;
+   private Mailbox currentMailbox;
+   private String currentRecording;
+   private String accumulatedKeys;
+   private Telephone phone;
+   private int state;
+
+   private static final int DISCONNECTED = 0;
+   private static final int CONNECTED = 1;
+   private static final int RECORDING = 2;
+   private static final int MAILBOX_MENU = 3;
+   private static final int MESSAGE_MENU = 4;
+   private static final int CHANGE_PASSCODE = 5;
+   private static final int CHANGE_GREETING = 6;
+   private static final int FORWARD_MESSAGE = 7;
+
+   private static final String INITIAL_PROMPT = 
+         "Enter mailbox number followed by #";      
+   private static final String MAILBOX_MENU_TEXT = 
+         "Enter 1 to listen to your messages\n"
+         + "Enter 2 to change your passcode\n"
+         + "Enter 3 to change your greeting";
+   private static final String MESSAGE_MENU_TEXT = 
+         "Enter 1 to listen to the current message\n"
+         + "Enter 2 to save the current message\n"
+         + "Enter 3 to delete the current message\n"
+         + "Enter 4 to forward the current message\n"
+         + "Enter 5 to return to the main menu";
