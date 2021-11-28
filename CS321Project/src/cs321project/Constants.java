1d276f5f917a9f9e7c1c9c36803e3aaab7ddf5ff
aem0424 <89554183+aem0424@users.noreply.github.com>
2021-11-28 12:56:03 -0600

    Add files via upload
    Made a slight correction to the Constants.java file.

--- CS321Project/src/cs321project/Constants.java
+++ CS321Project/src/cs321project/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
         public static final int CURRENT_MONTH = TODAY.get(Calendar.MONTH) + 1; // The Calender getMonth function is from 0-11, so increase by 1 to match input range
         public static final int CURRENT_YEAR = TODAY.get(Calendar.YEAR) - 2000; // Subtract by 2000 to match with Exp input data
         // PUT SOMETHING FOR FOOD_DATA FILEPATH (not finalized yet)
-        protected static final File FOOD_DATA = new File("C:\\Users\\antho\\Desktop\\CS 321-03\\cs321_group_project\\CS321Project\\src\\cs321project\\StorageAlt.xml");
+        protected static final File FOOD_DATA = new File("StorageAlt.xml");
         // PUT SOMETHING FOR FOOD_DATA FILEPATH (not finalized yet)
         protected static final int MAX_ITEMS = 5; // Total number of food items (5, for this project's purposes)
         protected static final int MAX_SIZES = 2; // Total number of different sizes available (2, for this project's purposes)        
